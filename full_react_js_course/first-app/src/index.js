5ddea42254a8ac6cb1d96eb204985b5b7d99a415
kalikokalikova <calico.goodrich@gmail.com>
2022-09-29 14:01:14 -0700

    Section 3 JSX

--- full_react_js_course/first-app/src/index.js
+++ full_react_js_course/first-app/src/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function getButtonText() {
+    return "I'm text delivered from a function!"
+}
+
 // create function-based component
 const App = () => {
-    return(
-        <div>Hello world</div>
+    const buttonText = {text: "Hello World"}
+
+    return( // a bunch of JSX
+        <div>
+            <div>Hello world</div>
+            <div>
+                              {/* make sure to use className instead of js keyword class */}
+                <label htmlFor="name" className="label">enter an email</label>
+                <input id="name" type="text" />
+                <button style={{ backgroundColor: 'green', color: 'white' }}>
+                              {/* this is a js object ^ */}
+                    { getButtonText() }
+                    {/* ^ This references a js function (variable) defined above */}
+                </button>
+            </div>
+            <div>
+                <button>
+                    { buttonText.text }
+                    {/* ^ This references a js object */}
+                </button>
+            </div>
+        </div>
     )
 }
 
@@ -13,3 +38,4 @@ ReactDOM.render(
     <App />, // component
     document.querySelector('#root') // DOM element to render into
 )
+
