c0d8d06a28a4154853873291c6937a4e040a6b8f
kalikokalikova <calico.goodrich@gmail.com>
2022-10-26 07:55:15 -0700

    get list from images from api

--- full_react_js_course/image-list/src/components/App.js
+++ full_react_js_course/image-list/src/components/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import SearchInput from './SearchInput';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
+        this.state = { images: [] }
+        this.onSearchSubmit = this.onSearchSubmit.bind(this)
+
     }
 
-    onSearchSubmit = (entry) => {
-        console.log(entry)
+    async onSearchSubmit(entry) {
+        await fetch(`https://pixabay.com/api/?key=30836534-8eb7e2070cfe19a8102bf982c&q=${entry}&image_type=photo`)
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({images: json.hits})
+            })
     }
 
     render() {
         return (
             <div className='ui container' style={{marginTop:'30px'}}>
                 <SearchInput onSearchSubmit={this.onSearchSubmit}/>
+                <ImageList images={this.state.images}/>
             </div>
         )
     }
